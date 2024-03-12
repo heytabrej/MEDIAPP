2ce0a80c89fbece849fda4379f1e4d2234219e71
KF Surya <129559924+Surya-KF@users.noreply.github.com>
2024-03-12 22:37:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def web_writer(a):
 
 
 example = web_writer("example")
-key = "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"
+key = "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"
 chatllm = ChatOpenAI(openai_api_key=key, temperature=0.8, model='gpt-3.5-turbo')
 
 
